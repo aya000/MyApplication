31ef14904384b17489cd65fc1830dce6c65e83da
aya000 <758332657@qq.com>
2017-12-18 10:38:47 +0800

    1、first commit

---
+++ 2/MyZXingTwo/app/src/main/java/com/example/myzxingtwo/BaseActivity.java
@@ -0,0 +1,62 @@
+package com.example.myzxingtwo;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+/**
+ * Created by YiBei on 2017/12/14.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme_NoActionBar);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){ //android 4.4
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);//状态栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+
+        //activity跳转动画
+        overridePendingTransition(R.anim.activity_open, 0);
+        
+        initView();
+        initListener();
+
+
+
+
+
+
+
+
+    }
+
+
+
+    private void initView() {}
+
+    private void initListener() {}
+
+    Toast toast = null;
+
+    protected void Toast(String s){
+        if(toast == null){
+            toast = Toast.makeText(this, s , Toast.LENGTH_LONG);
+            toast.show();
+        }else{
+            toast.setText(s);
+            toast.show();
+        }
+    }
+
+
+
+
+}
